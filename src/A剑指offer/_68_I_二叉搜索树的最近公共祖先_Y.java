36137da7883f76df7c78236e3de4ec3d9f354ef0
gaopeiwen <gaopeiwen@bytedance.com>
2021-02-03 17:56:21 +0800

    _68_I_二叉搜索树的最近公共祖先_Y
    Change-Id: Ibc3ef08411bed10b7ff904986a63ddcc18582014

--- src/A剑指offer/_68_I_二叉搜索树的最近公共祖先_Y.java
+++ src/A剑指offer/_68_I_二叉搜索树的最近公共祖先_Y.java
@@ -4,12 +4,49 @@ package A剑指offer;
  * https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-zui-jin-gong-gong-zu-xian-lcof/
  */
 public class _68_I_二叉搜索树的最近公共祖先_Y {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    /**
+     * 二叉树的最近公共祖先
+     */
+    public TreeNode lowestCommonAncestor1(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || root == p || root == q) return root;
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        TreeNode left = lowestCommonAncestor1(root.left, p, q);
+        TreeNode right = lowestCommonAncestor1(root.right, p, q);
         if (left == null) return right;
         if (right == null) return left;
+        return root;
+    }
+
+    /**
+     * 递归
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else {
+            return root;
+        }
+    }
+
+    /**
+     * 迭代
+     */
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val > q.val) {
+            TreeNode tmp = p;
+            p = q;
+            q = tmp;
+        }
+        while (root != null) {
+            if (root.val < p.val) {
+                root = root.right;
+            } else if (root.val > q.val) {
+                root = root.left;
+            } else {
+                break;
+            }
+        }
         return root;
     }
 }
