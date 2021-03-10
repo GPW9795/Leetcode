55e3adf88a76aef43948c8c2c0f234263aa85d43
gaopeiwen <gaopeiwen@bytedance.com>
2021-03-10 20:20:11 +0800

    修复age_gate_birthday的bug
    Change-Id: I8816a12298039b2445fa77e7da1940a5c86dc898

---
+++ src/A剑指offer/_26_树的子结构.java
@@ -0,0 +1,16 @@
+package A剑指offer;
+
+/**
+ * https://leetcode-cn.com/problems/shu-de-zi-jie-gou-lcof/
+ */
+public class _26_树的子结构 {
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        return (A != null && B != null) && ((helper(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
+    }
+
+    public boolean helper(TreeNode A, TreeNode B) {
+        if (B == null) return true;
+        if (A != null || A.val != B.val) return false;
+        return helper(A.left, B.left) && helper(A.right, B.right);
+    }
+}
--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,31 @@
 import java.util.Arrays;
-import java.util.PriorityQueue;
 
-class Solution {
-    static int x = 10;
-
-    static {
-        x += 5;
+public class Solution {
+    public static void helper(int[][] a) {
+        for (int i = 0; i < a.length; i++) { // 二维数组的长度
+            for (int j = 0; j < a[i].length; j++) { // 每个一维数组的长度
+                int n = j + 1;
+                for (int m = i; m < a.length; m++) {
+                    for (; n < a[i].length; n++) {
+                        if (a[i][j] > a[m][n]) {
+                            int min = a[m][n];
+                            a[m][n] = a[i][j];
+                            a[i][j] = min;
+                        }
+                    }
+                    n = 0; // 此处是给n从第二个一维数组开始取0这个坐标
+                }
+            }
+        }
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                System.out.println(a[i][j]);
+            }
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println("x = " + x);
-    }
-
-    static {
-        x /= 3;
+        int[][] A = {{3, 1, 2}, {4, 1, 3}};
+        helper(A);
     }
 }
