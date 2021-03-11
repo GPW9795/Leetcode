bca0a8efb4cdc9514f1df82d5b2072050e4711e6
gaopeiwen <gaopeiwen@bytedance.com>
2021-03-11 10:34:24 +0800

    hhh
    Change-Id: I174073e9a3c2949709ddb168bbac86fe18f5b701

--- src/A剑指offer/_26_树的子结构_Y.java
+++ src/A剑指offer/_26_树的子结构_Y.java
@@ -3,14 +3,14 @@ package A剑指offer;
 /**
  * https://leetcode-cn.com/problems/shu-de-zi-jie-gou-lcof/
  */
-public class _26_树的子结构 {
+public class _26_树的子结构_Y {
     public boolean isSubStructure(TreeNode A, TreeNode B) {
-        return (A != null && B != null) && ((helper(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
+        return (A != null && B != null) && (helper(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
     }
 
     public boolean helper(TreeNode A, TreeNode B) {
         if (B == null) return true;
-        if (A != null || A.val != B.val) return false;
+        if (A == null || A.val != B.val) return false;
         return helper(A.left, B.left) && helper(A.right, B.right);
     }
 }
