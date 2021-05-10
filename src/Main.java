1ba56911b06f2f1a811a76318ea6599183914f70
gaopeiwen <gaopeiwen@bytedance.com>
2021-05-10 11:48:14 +0800

    the last day in bytedance
    Change-Id: I8a7450cb1214a9af6914d1a8a67e5d107a26e362

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] nums = new int[n];
-        for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
-        }
-        int target = sc.nextInt();
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            int num = nums[i];
-            for (int j = i; j < n; j++) {
-                num |= nums[j];
-                if (num <= target) {
-                    res++;
-                }
-            }
-        }
-        res = res % 1000000007;
-        System.out.println(res);
+
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -2,45 +2,6 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int p = sc.nextInt();
-        int[][] arr = new int[m][2];
-        int[] degrees = new int[n + 1];
-        for (int i = 0; i < m; i++) {
-            arr[i][0] = sc.nextInt();
-            arr[i][1] = sc.nextInt();
-            degrees[arr[i][1]]++;
-        }
 
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        for (int i = 1; i <= n; i++) {
-            if (degrees[i] == 0) queue.offer(i);
-        }
-
-        int res = n;
-        boolean flag = true;
-        while (!queue.isEmpty() && flag) {
-            int cur = queue.poll();
-            if (cur == p) {
-                System.out.println(n);
-                break;
-            }
-            res--;
-            for (int[] a : arr) {
-                if (a[0] != cur) continue;
-                if (--degrees[a[1]] == 0) queue.offer(a[1]);
-                if (a[1] == p) {
-                    flag = false;
-                    break;
-                }
-            }
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= res; i++) {
-            sb.append(i);
-        }
-        System.out.println(sb.toString());
     }
 }
