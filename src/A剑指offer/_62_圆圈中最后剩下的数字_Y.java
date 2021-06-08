99510fc9589173c551f2a5169dd641256e40e40e
gaopeiwen <gaopeiwen.gpw@alibaba-inc.com>
2021-06-08 16:56:23 +0800

    jianzhi offer 62

--- src/A剑指offer/_62_圆圈中最后剩下的数字_Y.java
+++ src/A剑指offer/_62_圆圈中最后剩下的数字_Y.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 /**
  * https://leetcode-cn.com/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/
  */
-public class _62_圆圈中最后剩下的数字 {
+public class _62_圆圈中最后剩下的数字_Y {
     /**
      * ArrayList勉强通过，LinkedList超时
      */
@@ -27,7 +27,11 @@ public class _62_圆圈中最后剩下的数字 {
      * 约瑟夫环问题
      */
     public static int lastRemaining1(int n, int m) {
-        return 0;
+        int res = 0;
+        for (int i = 2; i <= n; i++) {
+            res = (m + res) % i;
+        }
+        return res;
     }
 
     public static void main(String[] args) {
